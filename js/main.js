1b8547f5ce979c60b4cfcfb9ccbc296068d0b8b5
djedynak259 <dajed@outlook.com>
2017-11-05 14:31:17 -0800

    slide in complete

--- js/main.js
+++ js/main.js
@@ -11,21 +11,8 @@ function showPage() {
 }
 
 
-// Nav jump
+// Button GoTo
 
-
-// document.getElementById('navAlignLeft').addEventListener('click', function(){
-// 	let el = document.getElementsByClassName('navButtonList')[0];
-// 	el.style.animationName= 'navFlexLeft';
-// })
-
-// document.getElementById('navAlignRight').addEventListener('click', function(){
-// 	let el = document.getElementsByClassName('navButtonList')[0];
-// 	el.style.animationName= 'navFlexRight';
-// })
-
-
-// Button Zooming
 document.getElementsByClassName('navBtn')[0].addEventListener('click',function(event){
 	let reccommendations = document.getElementById('recommendations');
 		reccommendations.scrollIntoView(true);
@@ -46,10 +33,24 @@ document.getElementsByClassName('navBtn')[4].addEventListener('click',function(e
 		contact.scrollIntoView(true);
 })
 
-// Scroll Nav
-var nav = document.getElementsByTagName('nav')[0];
-var navList = document.getElementsByClassName('navButtonList')[0]
-window.addEventListener('scroll', function (){
+// Scroll Effects
+
+function debounce(func, wait = 10, immediate = true) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+
+function checkScrollThings(){
 	if(document.body.scrollTop >= 10 || document.documentElement.scrollTop >= 10) {
 		nav.classList.add("navScrolled")
 		navList.classList.add("scrolled")
@@ -58,34 +59,32 @@ window.addEventListener('scroll', function (){
     nav.classList.remove("navScrolled");
   	navList.classList.remove("scrolled")  
 	}
-})
 
-// Slide In
-
-let sliders = document.querySelectorAll('.slide-in');
-console.log(sliders)
-
-window.addEventListener('scroll', function(){
 	sliders.forEach(elem =>{
 		let e = elem.getBoundingClientRect()
-		console.log(e, window.scrollY+ window.innerHeight)
-		let slideHeight = (window.scrollY + window.innerHeight)- 200 - e.height / 2;
-		let elemBottom = e.bottom;
+		
+		let slideHeight = (window.innerHeight) - 20;
 
-		let isHalf = slideHeight > e.top;
-		let isNotPast = window.scrollY < elemBottom;
-
-		if(isHalf && isNotPast) {
-			elem.classList.remove('slideAway')
-			// console.log('add')
-		}
-		else {
-			elem.classList.add('slideAway')
-			// console.log('remove')
+		if(slideHeight < e.top) {
+			elem.classList.add('slide-up')
 		}
 
+		if(slideHeight > e.top && e.bottom > 60) {
+			elem.classList.remove('slide-up')
+			elem.classList.remove('slide-down')
+		}
+
+		if(e.bottom < 60){
+			elem.classList.add('slide-down')
+		}
 	})
-})
+}
+
+let nav = document.getElementsByTagName('nav')[0];
+let navList = document.getElementsByClassName('navButtonList')[0]
+let sliders = document.querySelectorAll('.slide-in');
+
+window.addEventListener('scroll', debounce(checkScrollThings))
 
 
 // Modal  
@@ -96,23 +95,18 @@ var modal = document.getElementById('myModal');
 // Get the button that opens the modal
 var btn = document.getElementById("myModal1");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("modal-close")[0];
+var close = document.getElementsByClassName("modal-close")[0];
 
-// When the user clicks the button, open the modal 
 btn.onclick = function() {
     modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+close.onclick = function() {
     modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
 }
-// Modal  End
